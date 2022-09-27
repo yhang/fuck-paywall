82512adfd9c72389256bc61dd369e86b95e3ee36
yhang <chengweixing@gmail.com>
2022-09-27 09:40:30 +0800

    Update The Wall Street Journal Full Text Articles.user.js
    支持华尔街日报中文版

--- The Wall Street Journal Full Text Articles/The Wall Street Journal Full Text Articles.user.js
+++ The Wall Street Journal Full Text Articles/The Wall Street Journal Full Text Articles.user.js
@@ -1,14 +1,17 @@
 // ==UserScript==
-// @name         The Wall Street Journal Full Text Articles
-// @namespace    https://andrealazzarotto.com/
-// @version      1.0.3
-// @description  Fetch the full text of The Wall Street Journal from the AMP version
-// @author       Andrea Lazzarotto
-// @match        https://www.wsj.com/articles/*
-// @require      https://code.jquery.com/jquery-latest.min.js
-// @grant        GM_xmlhttpRequest
-// @grant        GM.xmlHttpRequest
-// @license      GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @name            The Wall Street Journal Full Text Articles
+// @name:it         The Wall Street Journal - Articoli con testo completo
+// @namespace       iamfredchu
+// @version         0.0.2
+// @description     Fetch the full text of The Wall Street Journal articles from the AMP version
+// @description:it  Mostra il testo completo degli articoli su The Wall Street Journal
+// @author          Fred Chu
+// @match           https://www.wsj.com/articles/*
+// @match           https://cn.wsj.com/articles/*
+// @require         https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js
+// @grant           GM_xmlhttpRequest
+// @grant           GM.xmlHttpRequest
+// @license         GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // ==/UserScript==
 
 /* Greasemonkey 4 wrapper */
@@ -27,9 +30,9 @@ function fetch(params) {
 (function() {
     'use strict';
 
-    var paywalled = $(".wsj-snippet-login").is(':visible');
+    var paywalled = $("#cx-snippet-promotion");
 
-    if (paywalled) {
+    if (!!paywalled) {
         fetch({
             method: 'GET',
             url: '/amp' + location.pathname,
@@ -37,9 +40,29 @@ function fetch(params) {
             var r = responseDetails.responseText;
             r = r.replace(/<script/gi, '<div').replace(/script>/gi, 'div>');
             r = r.replace(/\?width=/gi, '?__=').replace(/<amp-img/gi, '<img').replace(/<.amp-img>/, '').replace(/amp-iframe/gi, 'iframe');
+
             var data = $(r);
-            $('.wsj-snippet-body').replaceWith(data.find('[amp-access=access]').css('margin-bottom', '5rem'));
-            $('.wsj-snippet-login, .wsj-ad, .banner-ad-b, .media-object:not(:has(>*))').remove();
+
+            setTimeout(function(){
+                let hasSnippet = $('.wsj-snippet-body');
+                let $preview = $('.wsj-snippet-body').length ?
+                    $('.wsj-snippet-body') :
+                    $('[aria-label*="Listen"]').next('section');
+
+                    $preview.replaceWith(data.find('section[subscriptions-section="content"]')
+                                         .css('margin-bottom', '5rem')
+                                         .css('color', 'var(--primary-text-color)')
+                                         .css('font-family', 'var(--article-font-family)')
+                                         .css('font-size', 'calc((17 / var(--article-base-font-size)) * var(--article-text-size-scale) * 1rem)')
+                                         .css('font-weight', 'var(--article-font-weight)')
+                                         .css('line-height', 'calc(27 / 17)')
+                                        );
+
+            }, 3000);
+
+            $('[aria-label*="Listen"]').next().next().remove();
+            $('[aria-label*="What"]').remove();
+            $('.snippet-promotion, #cx-what-to-read-next').remove();
             $('.responsive-media').css('height', 'auto');
             $('.responsive-media img').css({
                 'height': 'auto',
